6ed4666e22bc25de131d16fba3db77cba81d2f7c
r34mod <48959634+r34mod@users.noreply.github.com>
2020-12-19 16:38:13 +0100

    Add files via upload

---
+++ models/colegio.py
@@ -0,0 +1,60 @@
+from odoo import api, fields, models, 
+import datetime
+
+class colegiocolegio(models.Model):
+        _name = 'colegio.colegio'
+        nombre = fields.Text('Nombre del centro', required=True)
+        id = fields.Char('Identificador' , required=True)
+        educacion= fields.Selection([
+            ('publica'),
+            ('privada'),
+            ('concertada')
+        ])
+        materiales = fields.Many2one('colegio.materiales', 'Proveedor de Materiales', required=True)
+
+
+
+
+class colegioprofesores(models.Model):
+    _name = 'colegio.profesores'
+
+    @api.depends('nacimiento', 'edad')
+
+    def _edad(self):
+            self.edad=datetime.date.today().year - self.nacimiento.year
+
+    nombreProfe = fields.Text('Nombre')
+    apellido = fields.Text('Apellido')
+    nacimiento = fields.Date('Año nacimiento', required=True, default=fields.Date.context_today)
+    edad = fields.Integer('Años', compute='_edad')
+    director = fields.Boolean('Director')
+    centro = fields.One2one('colegio.colegio', 'Centro', required=True)
+    aula = fields.One2one('colegio.aulas', 'Aulas', required=True)
+
+
+
+
+class colegioaulas(models.Model):
+        _name = 'colegio.aulas'
+
+        idAula = fields.Text('Aula', required=True)
+        profesorAula = fields.One2one('colegio.profesores', 'Profesor', required=True)
+        delegado = fields.One2one('colegio.alumnos', 'Delegado', required=True)
+        bilingue = fields.Boolean('Bilingue')
+
+
+
+
+class colegioprovedores(models.Model):
+        _name = 'colegio.provedores'
+
+         @api.depends('cantidad', 'prize')
+    def _equal(self):
+        self.equal = self.cantidad *self.prize *(26/100)
+        
+    marca = fields.Text('Marca', required=True)
+    modelo = fields.Text('Modelo', required=True)
+    cantidad = fields.Float(string='Cantidad')
+    prize = fields.Float(string='prize', required=True)
+    equal = fields.Float(string='equal', compute='_equal')
+    
